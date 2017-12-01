bf79fbc20adeb44085888edf0e5c6caac146b484
kimth <kimth@stanford.edu>
2017-12-01 11:57:47 -0800

    Simple laser toggle

---
+++ laser_pulse/laser_pulse.ino
@@ -0,0 +1,52 @@
+// Pinout
+#define USER_INPUT 52
+#define OUTPUT_TO_LASER 53
+
+// FSM definitions
+#define S_IDLE 0
+#define S_PULSE_HIGH 1
+#define S_PULSE_LOW 2
+int state;
+
+// Laser stim definitions
+#define PULSE_RATE 20 // Hz
+#define PULSE_ON_DURATION 5 // ms
+#define PULSE_OFF_DURATION (1000/PULSE_RATE - PULSE_ON_DURATION)
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(USER_INPUT, INPUT);
+  pinMode(OUTPUT_TO_LASER, OUTPUT);
+  state = S_IDLE;
+}
+
+void loop() {
+  bool in = digitalRead(USER_INPUT);
+
+  in = true; // constantly toggle
+  switch (state) { 
+    case S_IDLE:
+      digitalWrite(OUTPUT_TO_LASER, 0); // Keep laser off
+      if (in)
+        state = S_PULSE_HIGH;
+      break;
+
+    case S_PULSE_HIGH:
+      digitalWrite(OUTPUT_TO_LASER, 1);
+      delay(PULSE_ON_DURATION);
+      state = S_PULSE_LOW;
+      break;
+
+    case S_PULSE_LOW:
+      digitalWrite(OUTPUT_TO_LASER, 0);
+      delay(PULSE_OFF_DURATION);
+      if (in)
+        state = S_PULSE_HIGH;
+      else
+        state = S_IDLE;
+      break;
+
+    default:
+      state = S_IDLE;
+  }
+}
