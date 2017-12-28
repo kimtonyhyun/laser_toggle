db21e98336edcbb468f4d2df4c77f1298ab83c83
kimth <kimth@stanford.edu>
2017-12-28 10:23:57 -0800

    Toggle 15

---
+++ subframe_toggle15/subframe_toggle15.ino
@@ -0,0 +1,177 @@
+// Pinout
+#define FRAME_CLK 31 // Port C, pin 6
+#define LINE_CLK 33  // Port C, pin 4 
+#define MOD_IN 35    // Port C, pin 2
+#define LASER_OUT 37 // Port C, pin 0
+
+bool mod;
+bool frame, prev_frame;
+bool line, prev_line;
+
+// Use direct port manipulation for speed
+#define READ(x,y) (x&(1<<y))
+#define CLR(x,y) (x&=(~(1<<y)))
+#define SET(x,y) (x|=(1<<y))
+
+// Hard-code subframe parameters. Here, we:
+//  - Assume that the full frame is 512 lines
+//  - Split the full frame into N=15 separate "subframes", there are N-1 "lines" where the
+//      laser state will transition
+#define SUBFRAME_LINE1 35
+#define SUBFRAME_LINE2 69
+#define SUBFRAME_LINE3 103
+#define SUBFRAME_LINE4 137
+#define SUBFRAME_LINE5 171
+#define SUBFRAME_LINE6 205
+#define SUBFRAME_LINE7 239
+#define SUBFRAME_LINE8 273
+#define SUBFRAME_LINE9 307
+#define SUBFRAME_LINE10 341
+#define SUBFRAME_LINE11 375
+#define SUBFRAME_LINE12 409
+#define SUBFRAME_LINE13 443
+#define SUBFRAME_LINE14 447
+#define IM_LINE_OFFSET 2
+
+// Amount of time to delay for transition of laser at the end of frame. This
+//  is useful for dual-site imaging, where the slave microscope frames lag the
+//  master microscope frames by a few lines.
+#define TOGGLE_DELAY 300 // us
+
+#define S_IM1 0 // Image subfields 1, 3, 5
+#define S_IM2 1 // Image subfield 2, 4
+int imaging_state = S_IM1;
+int line_idx = 1;
+
+bool mod_enabled = false;
+
+void setup() {
+  pinMode(FRAME_CLK, INPUT);
+  pinMode(LINE_CLK, INPUT);
+  pinMode(MOD_IN, INPUT);
+  pinMode(LASER_OUT, OUTPUT);
+
+  frame = false;
+  line = false;
+}
+
+void loop() {
+  mod = READ(PINC,2);
+
+  prev_frame = frame;
+  frame = READ(PINC,6);
+  
+  prev_line = line;
+  line = READ(PINC,4);
+  
+
+  // First rising edge on FRAME clk after MOD_IN is high
+  if (mod && !mod_enabled && !prev_frame && frame)
+  {
+    mod_enabled = true;
+    
+    imaging_state = S_IM1;
+    line_idx = 1;
+  }
+
+  if (!mod_enabled)
+    CLR(PORTC,0); // Keep laser OFF
+  else
+  {
+    if (prev_frame && !frame) // Falling edge on FRAME clk
+    {
+      mod_enabled = mod;
+
+      delayMicroseconds(TOGGLE_DELAY);
+      
+      switch (imaging_state)
+      {
+        case S_IM1:
+          imaging_state = S_IM2;
+          if (mod_enabled)
+            SET(PORTC,0); // Laser ON
+          break;
+        case S_IM2:
+          imaging_state = S_IM1;
+          CLR(PORTC,0); // Laser OFF
+          break;
+      }
+      line_idx = 1;
+    }
+
+    if (!prev_line && line) // Rising edge on LINE clk
+    {
+      switch (imaging_state)
+      {
+        case S_IM1:
+          if (line_idx < SUBFRAME_LINE1 + IM_LINE_OFFSET)
+            CLR(PORTC,0); // Laser OFF
+          else if (line_idx < SUBFRAME_LINE2 - IM_LINE_OFFSET)
+            SET(PORTC,0); // Laser ON
+          else if (line_idx < SUBFRAME_LINE3 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE4 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE5 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE6 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE7 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE8 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE9 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE10 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE11 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE12 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE13 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE14 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else
+            CLR(PORTC,0); // Laser OFF
+          break;
+          
+        case S_IM2:
+          if (line_idx < SUBFRAME_LINE1 - IM_LINE_OFFSET)
+            SET(PORTC,0); // Laser ON
+          else if (line_idx < SUBFRAME_LINE2 + IM_LINE_OFFSET)
+            CLR(PORTC,0); // Laser OFF
+          else if (line_idx < SUBFRAME_LINE3 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE4 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE5 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE6 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE7 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE8 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE9 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE10 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE11 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE12 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE13 - IM_LINE_OFFSET)
+            SET(PORTC,0);
+          else if (line_idx < SUBFRAME_LINE14 + IM_LINE_OFFSET)
+            CLR(PORTC,0);
+          else
+            SET(PORTC,0); // Laser ON
+          break;
+      }
+
+      line_idx++;
+    }
+  }
+
+}
